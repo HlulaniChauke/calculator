89751d9879a1dcfca346f90d1ce6db6fa2686054
Hlulani Chauke <122245777+HlulaniChauke@users.noreply.github.com>
2023-11-12 22:14:49 +0900

    Fix displaying on numerals

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const sreenDisplay = document.querySelector("p");
 //make am array to store the entered number in the display;
 let arrDisplay = [];
 //make variable to store currentNumber;
-let currentNumber = 0;
-//make a variable to store currentResult;
 let currentResult = 0;
 //make function to add, multiply, subtract and divide
 
@@ -35,18 +33,15 @@ function simpleMath(currentResult, currentNumber, symbolChosen){
 
 //make clearAllVariable function
 function clearAllVariable(){
-    return currentResult = 0
+     currentResult = 0;
+     arrDisplay = [];
 }
 
 //make negateNumber function
-function negateNumber(currentNumber){
-    return currentNumber *= (-1);   
-}
+function negateNumber(currentNumber){return currentNumber *= (-1);   }
 
 //make calPercentage function
-function calPercentage(currentNumber){
-    return currentNumber/100
-}
+function calPercentage(currentNumber){return currentNumber/100}
 
 //make displayOnScreen function
 function displayOnScreen (num){
@@ -59,10 +54,18 @@ function displayOnScreen (num){
 
 }
 
-
 calValues.addEventListener('click',(e) => {
-    //((e.target.id));
-    displayOnScreen(e.target.innerHTML);
+    let actionToTake = e.target.className;
+    if (actionToTake === 'function'){
+
+    }
+    if (actionToTake === 'operands'){
+
+    }
+    if (actionToTake === 'numerals'){
+        displayOnScreen(e.target.innerHTML);   
+    }
+    
 });
 
 
