41547c46566ddeb50a34981dc2dc9296e0e34a2f
Hlulani Chauke <122245777+HlulaniChauke@users.noreply.github.com>
2023-11-13 19:50:08 +0900

    Restarted file Create display to screen function

--- script.js
+++ script.js
@@ -1,77 +1,23 @@
-//this has 19 elements.(0-18)
-const calValues = document.querySelector(".keypad");
-const sreenDisplay = document.querySelector("p");
-//make am array to store the entered number in the display;
-let arrDisplay = [];
-//make variable to store currentNumber;
-let currentResult = 0;
-//make function to add, multiply, subtract and divide
+const calSreen = document.querySelector('p');
+const numerals = document.querySelector('.keypad')
+let arryDisplayNum = [];
 
-function simpleMath(currentResult, currentNumber, symbolChosen){
-    if (symbolChosen === '/' && currentNumber === 0){ 
-        return "Err"
+function screenDisplay(num){
+    arryDisplayNum.push(num);
+    let displayNum = arryDisplayNum;
+    if (displayNum.length > 0){
+        displayNum = displayNum.slice(0,14);
     }
-    if(symbolChosen === '/'){
-        currentResult = currentResult / currentNumber;
-    }
-
-    if(symbolChosen === '*'){
-        currentResult = currentResult * currentNumber;
-    }
-
-    if(symbolChosen === '-'){
-        currentResult = currentResult - currentNumber;
-    }
-
-    if(symbolChosen ==='+'){
-        currentResult = currentResult + currentNumber;
-    }
-    
-    return currentResult
-
+    calSreen.innerHTML = displayNum.join('');
 }
 
-//make clearAllVariable function
-function clearAllVariable(){
-     currentResult = 0;
-     arrDisplay = [];
-     sreenDisplay.innerHTML = 0;
+function displayNumerals(value){
+    console.log(value);
+    screenDisplay(value);
 }
 
-//make negateNumber function
-function negateNumber(currentNumber){return currentNumber *= (-1);   }
-
-//make calPercentage function
-function calPercentage(currentNumber){return currentNumber/100}
-
-//make displayOnScreen function
-function displayOnScreen (num){
-    arrDisplay.push(num);
-    let toDisplay = arrDisplay;
-    if (toDisplay.length > 9){
-        toDisplay = toDisplay.slice(0,9);
-    }
-    sreenDisplay.innerHTML = toDisplay.join('');
-
+numerals.addEventListener('click', (e)=> {
+    let value = e.target.innerHTML;
+    displayNumerals(value);
 }
-
-calValues.addEventListener('click',(e) => {
-    let actionToTake = e.target.className;
-    if (actionToTake === 'functions'){
-        if (e.target.id === 'c'){
-            console.log(e.target.id);
-            clearAllVariable();
-        }
-
-    }
-    if (actionToTake === 'operands'){
-        displayOnScreen(e.target.innerHTML);   
-    }
-    if (actionToTake === 'numerals'){
-        displayOnScreen(e.target.innerHTML);   
-    }
-    
-});
-
-
-//make floatingPoint function
+)
