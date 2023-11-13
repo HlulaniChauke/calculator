65619d93911be40b8f91bbe74906ee106706a04b
Hlulani Chauke <122245777+HlulaniChauke@users.noreply.github.com>
2023-11-13 20:28:42 +0900

    Fix floating number display

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const calSreen = document.querySelector('p');
 const numerals = document.querySelector('.keypad')
 let arryDisplayNum = [];
 
-function screenDisplay(num){
-    arryDisplayNum.push(num);
+function screenDisplay(value){
+    if (value === "." && arryDisplayNum.includes(".")){
+        arryDisplayNum = arryDisplayNum;
+    }else {
+        arryDisplayNum.push(value);
+    }
+   
     let displayNum = arryDisplayNum;
     if (displayNum.length > 0){
         displayNum = displayNum.slice(0,14);
@@ -13,11 +18,18 @@ function screenDisplay(num){
 
 function displayNumerals(value){
     console.log(value);
-    screenDisplay(value);
+    if(arryDisplayNum.length === 0 && value === "0"){
+        calSreen.innerHTML = 0;
+    } else{
+        screenDisplay(value);
+    }   
 }
 
 numerals.addEventListener('click', (e)=> {
     let value = e.target.innerHTML;
-    displayNumerals(value);
+    if(e.target.className === 'numerals'){
+        displayNumerals(value);
+    }
+        
 }
 )
